c39e1ebe4cb0524e18b8722b8a7e92b9933fccc5
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2024-03-03 12:51:21 +0100

    show command

--- plugin/compile.lua
+++ plugin/compile.lua
@@ -3,3 +3,4 @@ local compile = require('compile')
 
 vim.api.nvim_create_user_command('Compile', compile.compile, {})
 vim.api.nvim_create_user_command('CompileClose', compile.close, {})
+vim.api.nvim_create_user_command('CompileShow', compile.show, {})
